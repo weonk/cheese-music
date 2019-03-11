b1dd40cbd3ec37d03ad2792a7224011c2f10a58d
iamchriswu <18855032030@163.com>
2019-03-11 12:49:50 +0800

    点歌台后台实现，修复歌曲同步模块BUG

--- src/main/java/com/chris/cheese/cheesemusic/controller/song/SongRestController.java
+++ src/main/java/com/chris/cheese/cheesemusic/controller/song/SongRestController.java
@@ -1,10 +1,28 @@
 package com.chris.cheese.cheesemusic.controller.song;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("song")
 public class SongRestController {
+
+    private String songStatusMessage;
+
+    @PostMapping("/sendStatus")
+    public String sendSongStatus(String message) {
+        if (message != null && message.length() > 0) {
+            this.songStatusMessage = message;
+            return "success";
+        }
+        return "fail";
+    }
+
+    @GetMapping("/getStatus")
+    public String getSongStatus() {
+        return this.songStatusMessage;
+    }
 
 }
